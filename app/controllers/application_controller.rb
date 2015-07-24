7cbc13bc5536f77df46abf0f251866236b1f5104
hartrapaport <hartrapaport@gmail.com>
2015-07-24 18:24:50 +0000

    Second update

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+require_relative "../../config/environment"
+require_relative "../models/user.rb"
+require_relative "../models/project.rb"
+require 'sinatra'
+class ApplicationController < Sinatra::Base
+  
+  set :views, "app/views"
+  set :public, "public"
+  
+  
+  get "/" do
+      erb :index
+  end
+  post '/project' do
+    @project = Project.new({:project_name => params[:project_name], :genre => params[:genre], :rewards => params[:rewards], :goal => params[:goal], :days => params[:days], :owner => params[:owner]})
+    @project.save
+    erb :project
+  end
+  post '/project_sign_up' do
+    erb :project_sign_up
+  end
+  
+end
+  
